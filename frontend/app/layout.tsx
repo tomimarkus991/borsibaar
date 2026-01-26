0d352c99437532dfc59aa9a14ef3cf0ae44b8f35
Ingrid Lepik <ingridlepik@Ingrids-MacBook-Air.local>
2026-01-26 20:29:27 +0200

    feat: mount global Toaster

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,17 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+           <Toaster
+                position="bottom-center"
+                toastOptions={{
+                  duration: 5000,
+                  style: {
+                    maxWidth: "720px",
+                    whiteSpace: "pre-wrap",
+                    wordBreak: "break-word"
+                  },
+                }}
+              />
       </body>
     </html>
   );
