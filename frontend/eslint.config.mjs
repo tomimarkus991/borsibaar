f71d2f6e18996963ec5a8eeaeb762aaf260b6a8e
Tomi Markus Alber <tomimarkusalber@gmail.com>
2026-01-29 11:19:39 +0200

    chore: turn off no any rule in eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,6 +5,11 @@ import nextTs from 'eslint-config-next/typescript'
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+    {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
   globalIgnores([
     "node_modules/**",
     '.next/**',
